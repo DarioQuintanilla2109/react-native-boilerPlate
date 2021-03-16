3a9493c8d6dd26db235c285bb5cbdb4416511a9e
Dario_Quintanilla <DarioQuintanilla2@gmail.com>
2021-03-16 02:55:45 -0400

    refactored theme to its own file

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-// In App.js in a new project
-
-import React, { useEffect } from 'react'
-import { View, Text, Button, TextInput, Image } from 'react-native'
+import React from 'react'
+import { View, Text, Button } from 'react-native'
 import {
   NavigationContainer,
   DefaultTheme,
@@ -9,127 +7,8 @@ import {
   useTheme,
 } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { MIAMI } from './src/images/ma'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { createDrawerNavigator } from '@react-navigation/drawer'
 import { AppearanceProvider, useColorScheme } from 'react-native-appearance'
-// function DetailsScreen({ route, navigation }) {
-//   //getting params passed in
-//   const { itemId, otherParam } = route.params
-
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text>Details Screen</Text>
-//       <Text>itemId: {JSON.stringify(itemId)}</Text>
-//       <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-//       <Button
-//         title='Go to Details... again'
-//         onPress={() =>
-//           navigation.push('Details', {
-//             itemId: Math.floor(Math.random() * 100),
-//           })
-//         }
-//       />
-//       <Button title='Go back' onPress={() => navigation.goBack()} />
-//       <Button
-//         title='Go back to first screen in stack'
-//         onPress={() => navigation.popToTop()}
-//       />
-//     </View>
-//   )
-// }
-
-//we get data passed in from route.params.nameOfObjectPassedIn
-
-// function HomeScreen({ route, navigation }) {
-//   const [count, setCount] = React.useState(0)
-
-//   React.useLayoutEffect(() => {
-//     navigation.setOptions({
-//       headerRight: () => (
-//         <Button onPress={() => setCount(c => c + 1)} title='Update count' />
-//       ),
-//     })
-//   })
-
-//   return (
-//     <View>
-//       <Button
-//         onPress={() => {
-//           navigation.navigate('CreatePost')
-//         }}
-//         title='createPost'
-//       />
-//       <Text>Count: {count}</Text>
-//     </View>
-//   )
-// }
-
-// function CreatePostScreen({ navigation, route }) {
-//   const [postText, setPostText] = React.useState('')
-//   //getting params passed in
-
-//   return (
-//     <>
-//       <TextInput
-//         multiline
-//         placeholder="What's on your mind?"
-//         style={{ height: 200, padding: 10, backgroundColor: 'white' }}
-//         value={postText}
-//         onChangeText={setPostText}
-//       />
-
-//       <Button
-//         title='Done'
-//         onPress={() => {
-//           // Pass params back to home screen
-//           navigation.navigate('Home', { post: postText })
-//         }}
-//       />
-//     </>
-//   )
-// }
-
-function LogoTitle() {
-  return <Image style={{ width: 50, height: 50 }} source={MIAMI} />
-}
-
-// //2 properties getting returned, Screen, Navigator
-// const Stack = createStackNavigator()
-
-// function StackScreen() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen
-//           name='Home'
-//           component={HomeScreen}
-//           options={{
-//             headerTitle: props => <LogoTitle {...props} />,
-//             headerLeft: props => (
-//               <LogoTitle
-//                 {...props}
-//                 onPress={() => {
-//                   navigation.navigate('CreateScreen')
-//                 }}
-//               />
-//             ),
-//           }}
-//         />
-//         <Stack.Screen
-//           options={{
-//             headerTitle: props => <LogoTitle {...props} />,
-//             headerBackTitle: ' ',
-//             headerBackImage: props => <LogoTitle />,
-//           }}
-//           name='CreatePost'
-//           component={CreatePostScreen}
-//         />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   )
-// }
-///////////////////////////////////////////////////////////////////////////////////////
+import DK from './darkTheme'
 
 function HomeScreen({ navigation }) {
   return (
@@ -185,19 +64,10 @@ function MainStackScreen() {
 
 function App() {
   const scheme = useColorScheme()
-  const MyDarkTheme = {
-    dark: true,
-    colors: {
-      primary: '#9933ff',
-      background: '#00023',
-      card: '#000028',
-      text: '#ffffff',
-      border: '#000028',
-      notification: '#9933ff',
-    },
-  }
+  const MyDarkTheme = DK()
+
   return (
-    <NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <NavigationContainer theme={scheme === 'dark' ? MyDarkTheme : DefaultTheme}>
       <RootStack.Navigator mode='modal' headerMode='none'>
         <RootStack.Screen name='Main' component={MainStackScreen} />
         <RootStack.Screen name='MyModal' component={ModalScreen} />
---
+++ darkTheme.js
@@ -0,0 +1,17 @@
+//can choose any color theme you like!
+const myDarkTheme = () => {
+  const MyDarkTheme = {
+    dark: true,
+    colors: {
+      primary: '#9933ff',
+      background: '#000023',
+      card: '#000028',
+      text: '#ffffff',
+      border: '#000028',
+      notification: '#9933ff',
+    },
+  }
+  return MyDarkTheme
+}
+
+export default myDarkTheme
